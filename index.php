b748c97894c2e831077fdd66d6570e4d46cbcbbd
jhygt46 <infolifeoperacional@gmail.com>
2019-08-26 18:34:34 -0400

    aa

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $core = new Core();
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <title>BUENA NELSON</title>
+        <title>ARTEMEDICI</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
