1ee6f524088544c618ebc0990639351411b9181b
jhygt46 <infolifeoperacional@gmail.com>
2019-08-30 16:04:27 -0400

    aa

--- admin/install.php
+++ admin/install.php
@@ -3,6 +3,8 @@
 require ('class/config.php');
 $con = new mysqli($db_host[0], $db_user[0], $db_password[0]);
 
+echo $db_host[0]."/".$db_user[0]."/".$db_password[0]."<br/>";
+
 echo "<pre>";
 print_r($con);
 echo "</pre>";
