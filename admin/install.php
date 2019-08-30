49cfba887b87b2ae7af25a364047ae8ae3dc8620
jhygt46 <infolifeoperacional@gmail.com>
2019-08-30 16:01:26 -0400

    aa

--- admin/install.php
+++ admin/install.php
@@ -1,7 +1,7 @@
 <?php
 
 require ('class/config.php');
-$con = new mysqli($db_host, $db_user, $db_password);
+$con = new mysqli($db_host[0], $db_user[0], $db_password[0]);
 
 $tablas[0]['nombre'] = 'excepciones';
 $tablas[0]['campos'][0]['nombre'] = 'id_exc';
