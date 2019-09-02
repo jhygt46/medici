e7beb9dd654d7d6da445bbe38ac9dce6bc8fa25a
jhygt46 <infolifeoperacional@gmail.com>
2019-09-02 11:10:25 -0400

    aa

--- admin/install.php
+++ admin/install.php
@@ -12,8 +12,8 @@ require_once DIR."db.php";
 require_once DIR_BASE."config/config.php";
 $con = new mysqli($db_host[0], $db_user[0], $db_password[0]);
 
-//echo $db_password[0]."<br/>";
-//exit;
+echo $db_password[0]."<br/>";
+exit;
 
 $tablas[0]['nombre'] = 'usuarios';
 $tablas[0]['campos'][0]['nombre'] = 'id_usr';
