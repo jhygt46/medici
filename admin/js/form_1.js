afd4b090396e41c84782e44ea0f4d30a17e7b7c5
jhygt46 <infolifeoperacional@gmail.com>
2019-09-02 18:00:48 -0400

    aa

--- admin/js/form_1.js
+++ admin/js/form_1.js
@@ -255,17 +255,19 @@ function confirm(message){
         title: 'Are you sure?',
         text: "You won't be able to revert this!",
         type: 'warning',
+        footer: '',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
         confirmButtonText: 'Yes, delete it!'
     }).then((result) => {
-        if (result.value) {
-            Swal.fire(
-                'Deleted!',
-                'Your file has been deleted.',
-                'success'
-            )
+        if(result.value) {
+            Swal.fire({
+                title: 'Deleted!',
+                text: 'Your file has been deleted.',
+                type: 'success',
+                timer: 1000
+            })
         }
     })
 
