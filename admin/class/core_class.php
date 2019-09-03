26d72f3633c08b6c9c4a6c737a220359fa7df7b7
jhygt46 <infolifeoperacional@gmail.com>
2019-09-03 18:37:44 -0400

    aa

--- admin/class/core_class.php
+++ admin/class/core_class.php
@@ -483,7 +483,7 @@ class Core{
     }
     public function exp_servicios($id){
         if($sql = $this->con->prepare("SELECT id_ser FROM excepcion_servicios WHERE id_exc=?")){
-            if($sql->bind_param("iii", $id, $this->id_usr, $this->eliminado)){
+            if($sql->bind_param("i", $id)){
                 if($sql->execute()){
                     return $sql->get_result()->fetch_all(MYSQLI_ASSOC);
                 }else{ return htmlspecialchars($sql->error); }
