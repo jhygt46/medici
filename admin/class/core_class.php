74acd837d48b6df00bb19427a230235bb02d0d4d
jhygt46 <infolifeoperacional@gmail.com>
2019-09-03 17:30:11 -0400

    aa

--- admin/class/core_class.php
+++ admin/class/core_class.php
@@ -494,7 +494,7 @@ class Core{
 
         $fecha = date("Y-m-d", time() - 60*60*24);
         if($sql = $this->con->prepare("SELECT DISTINCT fecha FROM excepciones WHERE id_usr=? AND fecha>? AND eliminado=? ORDER BY fecha")){
-            if($sql->bind_param("isi", $this->id_usr, $fecha, $this->id_usr)){
+            if($sql->bind_param("isi", $this->id_usr, $fecha, $this->eliminado)){
                 if($sql->execute()){
                     return $sql->get_result()->fetch_all(MYSQLI_ASSOC);
                 }else{ return htmlspecialchars($sql->error); }
