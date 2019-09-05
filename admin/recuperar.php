82d07dd8d0f192f545ae740dd4075e627c4e7a18
jhygt46 <infolifeoperacional@gmail.com>
2019-09-05 19:08:20 -0400

    aa

--- admin/recuperar.php
+++ admin/recuperar.php
@@ -1,13 +1,17 @@
+<?php 
+    unset($_COOKIE);
+    session_destroy();
+?>
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#" lang="es-CL">
     <head>
         <title></title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel='shortcut icon' type='image/x-icon' href='<?php echo $info["path"]; ?>/images/favicon/<?php echo $info["favicon"]; ?>' />
+        <link rel='shortcut icon' type='image/x-icon' href='/images/favicon/<?php echo $info["favicon"]; ?>' />
         <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-        <script type="text/javascript" src="/salud_mental/admin/js/ingreso.js"></script>
-        <link rel="stylesheet" href="/salud_mental/admin/css/login.css" type="text/css" media="all">
+        <script type="text/javascript" src="js/ingreso.js"></script>
+        <link rel="stylesheet" href="css/login.css" type="text/css" media="all">
         <script>
             $(document).on('keypress',function(e){
                 if(e.which == 13){
