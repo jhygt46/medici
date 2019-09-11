3b54941a89429a074e1ad72bc0a3da66b74bc9a3
jhygt46 <infolifeoperacional@gmail.com>
2019-09-11 18:58:05 -0300

    aa

--- js/base.js
+++ js/base.js
@@ -1209,7 +1209,7 @@ function create_servicios_li(doctor){
     var data = create_element_class("ser_data");
     var cont_data = create_element_class("cont_ser_data valign");
     var titulo = create_element_class_inner("ser_titulo", "ÁREA PSIQUIATRICA");
-    var cont_ser_doc = create_element_class("cont_ser_doc");
+    var cont_ser_doc = create_element_class("cont_ser_doc clearfix");
 
     for(var i=0, ilen=doctor.lista_servicios.length; i<ilen; i++){
         console.log(doctor.lista_servicios[i].nombre);
