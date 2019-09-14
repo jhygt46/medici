56da1a381f751a54ad9b03c0573f0b16d3613be9
jhygt46 <infolifeoperacional@gmail.com>
2019-09-14 15:26:43 -0300

    aa

--- admin/class/guardar_class.php
+++ admin/class/guardar_class.php
@@ -330,13 +330,14 @@ class Guardar{
             $nombre = $_POST['nombre'];
             $correo = $_POST['correo'];
             $tipo = $_POST['tipo'];
+            $titulo = $_POST['titulo'];
             $pass1 = $_POST['pass1'];
             $pass2 = $_POST['pass2'];
 
             if($id > 0){
 
                 $sqlu = $this->con->prepare("SELECT id_usr FROM usuarios WHERE correo=? AND eliminado=?");
-                $sqlu->bind_param("sii", $correo, $this->eliminado);
+                $sqlu->bind_param("si", $correo, $this->eliminado);
                 $sqlu->execute();
                 $res = $sqlu->get_result();
                 
@@ -362,11 +363,11 @@ class Guardar{
 
                 }elseif($res->{"num_rows"} == 0){
 
-                    $sql = $this->con->prepare("UPDATE usuarios SET nombre=?, correo=?, tipo=? WHERE id_usr=? AND eliminado=?");
+                    $sql = $this->con->prepare("INSERT INTO usuarios SET nombre=?, correo=?, tipo=? WHERE id_usr=? AND eliminado=?");
                     $sql->bind_param("ssiii", $nombre, $correo, $tipo, $id, $this->eliminado);
                     if($sql->execute()){
                         $info['op'] = 1;
-                        $info['mensaje'] = "Medico modificado exitosamente";
+                        $info['mensaje'] = "Medico ingresado exitosamente";
                     }else{
                         $info['op'] = 2;
                         $info['mensaje'] = "Error: Permisos A2";
