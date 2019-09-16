ed4dcb78112570c9eaa05ed3f73b740e11efdacf
jhygt46 <infolifeoperacional@gmail.com>
2019-09-15 23:03:39 -0300

    aa

--- admin/class/core_class.php
+++ admin/class/core_class.php
@@ -278,7 +278,7 @@ class Core{
                                     $now_fin = $now_ini + $tiempo;
                                     $str_hr1 = (intval($now_ini/60) < 10) ? intval($now_ini/60) : "0".intval($now_ini/60) ;
                                     $str_hr2 = (intval($now_ini%60) < 10) ? intval($now_ini%60) : "0".intval($now_ini%60) ;
-                                    $str_hora = $str_hr1.":".$str_hr2.":00";
+                                    $str_hora = $str_hr1."*".$str_hr2.":00";
 
                                     if($sqlexc = $this->con->prepare("SELECT * FROM excepciones t1, excepcion_servicios t2 WHERE t1.id_usr=? AND t1.fecha=? AND t1.id_exc=t2.id_exc AND t2.id_ser=?")){
                                         if($sqlexc->bind_param("isi", $id_usr, $fecha, $id_ser)){
