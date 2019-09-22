76da03fd698fe00fb9e8810259b3337447164bb6
jhygt46 <infolifeoperacional@gmail.com>
2019-09-22 16:17:19 -0300

    aa

--- index.php
+++ index.php
@@ -4,11 +4,13 @@
 if(isset($_POST['token'])) {
 
     $url = 'https://www.google.com/recaptcha/api/siteverify';
+    
     $data = [
         'secret' => '6Lfor7kUAAAAAH-BQ5sqjnCyvBlBWSgNZ-ec8rx0',
         'response' => $_POST['token'],
         'remoteip' => $_SERVER['REMOTE_ADDR']
     ];
+
     $options = array(
         'http' => array(
             'header'  => 'Content-type: application/x-www-form-urlencoded\r\n',
@@ -16,6 +18,7 @@ if(isset($_POST['token'])) {
             'content' => http_build_query($data)
         )
     );
+
     $context  = stream_context_create($options);
     $response = file_get_contents($url, false, $context);
     $res = json_decode($response, true);
