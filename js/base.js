fdfafdda45ebbe8d02ec78264b9579fbed4b99dc
jhygt46 <infolifeoperacional@gmail.com>
2019-09-24 14:04:58 -0300

    aa

--- js/base.js
+++ js/base.js
@@ -27,7 +27,7 @@ function inicio(){
 
     grecaptcha.ready(function(){
         grecaptcha.execute('6Lfor7kUAAAAABomMyYcaO0RhvHJBmPF85PrNP2v', { action: 'homepage' }).then(function(token){            
-            document.getElementById('token_contacto').value = token;
+            //document.getElementById('token_contacto').value = token;
             document.getElementById('token_reserva').value = token;
         });
     });
