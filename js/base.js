0a8c8699b205a5bfdb595edeb49833eccf726c05
jhygt46 <infolifeoperacional@gmail.com>
2019-09-26 12:05:13 -0300

    aa

--- js/base.js
+++ js/base.js
@@ -1248,14 +1248,12 @@ function html_detalle(){
             var dtl_descripcion = create_element_class_inner('dtl_descripcion', data.servicios[i].descripcion);
             cont_dtl.appendChild(dtl_titulo);
             cont_dtl.appendChild(dtl_descripcion);
-            console.log(data.servicios[i]);
             for(var j=0, jlen=data.servicios[i].lista_doctores.length; j<jlen; j++){
                 if(data.servicios[i].lista_doctores[j].id == reserva.doctor){
-                    console.log(data.servicios[i].lista_doctores[j]);
                     //var dtl_doctor = create_element_class_inner('dtl_doctor', data.servicios[i].lista_doctores[j].nombre);
                     var dtl_precio = create_element_class_inner('dtl_precio', "Costo: " + formatNumber.new(parseInt(data.servicios[i].lista_doctores[j].precio), "$"));
                     var dtl_tiempo = create_element_class_inner('dtl_tiempo', "Duracion: " + data.servicios[i].lista_doctores[j].tiempo_min + " minutos");
-                    var dtl_html1 = create_element_class_inner('dtl_tiempo', data.servicios[i].lista_doctores[j].html1);
+                    var dtl_html1 = create_element_class_inner('dtl_html1', data.servicios[i].lista_doctores[j].html1);
                     //cont_dtl.appendChild(dtl_doctor);
                     cont_dtl.appendChild(dtl_precio);
                     cont_dtl.appendChild(dtl_tiempo);
