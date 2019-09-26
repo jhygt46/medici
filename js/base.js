8e1a9554d7757ff2facfb84edbd88c759eeb87d4
jhygt46 <infolifeoperacional@gmail.com>
2019-09-26 10:48:00 -0300

    aa

--- js/base.js
+++ js/base.js
@@ -1543,22 +1543,3 @@ function reserva_blank(){
 function set_reserva(reserva){
     localStorage.setItem("reserva", JSON.stringify(reserva));
 }
-var formatNumber = {
-    separador: ".", // separador para los miles
-    sepDecimal: ',', // separador para los decimales
-    formatear: function(num){
-        num +='';
-        var splitStr = num.split('.');
-        var splitLeft = splitStr[0];
-        var splitRight = splitStr.length > 1 ? this.sepDecimal + splitStr[1] : '';
-        var regx = /(\d+)(\d{3})/;
-        while (regx.test(splitLeft)) {
-            splitLeft = splitLeft.replace(regx, '$1' + this.separador + '$2');
-        }
-        return this.simbol + splitLeft +splitRight;
-    },
-    new: function(num, simbol){
-        this.simbol = simbol ||'';
-        return this.formatear(num);
-    }
-}
